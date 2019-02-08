0a8cf51e13343c654d502ea7dd084237861ae018
Student <jblazusi@ualberta.ca>
2019-02-07 20:43:55 -0500

    final push

--- publishconf.py
+++ publishconf.py
@@ -11,13 +11,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://blazusiak.github.io/CMPUT404LAB5/'
 RELATIVE_URLS = False
 
 THEME = 'templates/mytheme'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
