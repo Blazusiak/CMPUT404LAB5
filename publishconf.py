6e9da25bf0312e337aafade7715dc2bc85f43e60
Student <jblazusi@ualberta.ca>
2019-02-07 20:15:45 -0500

    Themes

--- publishconf.py
+++ publishconf.py
@@ -14,6 +14,8 @@ from pelicanconf import *
 SITEURL = ''
 RELATIVE_URLS = False
 
+THEME = 'templates/mytheme'
+
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
